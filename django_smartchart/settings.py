f94e262831ac11d851a2c602f10cc64d7beb2e4b
JohnYan <84345999@qq.com>
2020-12-10 21:35:46 +0800

    add template

--- django_smartchart/settings.py
+++ django_smartchart/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'django_smartchart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
