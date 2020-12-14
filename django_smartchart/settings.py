2fdcb72bf2aac23c58763a11f3988e3ab8636926
JohnYan <84345999@qq.com>
2020-12-14 16:57:00 +0800

    update django_smartchart/settings.py.

--- django_smartchart/settings.py
+++ django_smartchart/settings.py
@@ -119,6 +119,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
